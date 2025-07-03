7d164c4cb8af1f138674eca82f0f3570bee7474d
Mindul <pencake33@naver.com>
2025-07-03 13:18:35 +0900

    gitignore 추가

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.thefirsttake.app',
   appName: 'thefirsttake_frontend',
-  webDir: 'out'
+  server: {
+    hostname: 'the-first-take-frontend.vercel.app',
+    androidScheme: 'https',
+  },
 };
 
 export default config;
