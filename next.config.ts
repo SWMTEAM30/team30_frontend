87358924d67d846fbc11829c3d86d1a923ebab32
Mindul <pencake33@naver.com>
2025-07-07 14:38:23 +0900

    임시로 ci 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         // source: 이 경로로 들어오는 요청을
         source: '/api/:path*',
         // destination: 이 주소로 대신 보내준다
-        destination: `${process.env.NEXT_PUBLIC_TFT_BACKEND_URL}/api/:path*`,
+        destination: `http://3.39.213.27:8000/api/:path*`,
       },
     ];
   },
