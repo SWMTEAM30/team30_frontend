be37fe3d13d87dc81d6d14b706605b87eadf0ff4
Mindul <pencake33@naver.com>
2025-07-29 16:04:56 +0900

    wiki static page route 문제 해결

--- app/wiki/[...slug]/page.tsx
+++ app/wiki/[...slug]/page.tsx
@@ -1,17 +1,11 @@
 import { getAllStaticPaths, getMDXContent } from '@/lib/static-page';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
-/**
- * Next.js가 빌드 시점에 이 함수를 실행하여,
- * 어떤 정적 페이지들을 미리 생성해야 할지 목록을 받아갑니다.
- * 최종 반환값 예시: [{ slug: ['style', 'classic'] }, { slug: ['material', 'denim'] }]
- */
-
 export async function generateStaticParams() {
   return getAllStaticPaths('content/wiki');
 }
 
-export default async function WikiPage({ params }: { params: { slug: string[] } }) {
+export default async function WikiPage({ params }: any) {
   const { content, frontmatter } = getMDXContent('content/wiki', params.slug);
 
   return (
